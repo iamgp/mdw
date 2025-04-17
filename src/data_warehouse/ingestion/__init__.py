7ab97bf6e816eb71451f6011d9e9858a1e49743e
Gareth Price <g.price@oxb.com>
2025-04-17 21:51:59 +0100

    chore(ingestion): export ExampleDBSource for named import usage

--- src/data_warehouse/ingestion/__init__.py
+++ src/data_warehouse/ingestion/__init__.py
@@ -1,2 +1,4 @@
 from .dlt_pipeline import run_dlt_pipeline  # noqa: F401
+from .example_db_source import ExampleDBSource  # noqa: F401
+from .example_file_source import ExampleFileSource  # noqa: F401
 from .source_base import SourceBase  # noqa: F401
